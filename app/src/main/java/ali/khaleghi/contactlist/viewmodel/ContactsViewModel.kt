5f45fc24a9bb06d3999672f5954445d3237e2a9d
Ali Khaleghi <alikhaleghi76@gmail.com>
2019-11-12 22:05:04 +0330

    implement contacts view model

---
+++ app/src/main/java/ali/khaleghi/contactlist/viewmodel/ContactsViewModel.kt
@@ -0,0 +1,26 @@
+package ali.khaleghi.contactlist.viewmodel
+
+import ali.khaleghi.contactlist.data.db.entity.Contact
+import ali.khaleghi.contactlist.data.repository.ContactsRepository
+import androidx.lifecycle.LiveData
+
+class ContactsViewModel(private var repository: ContactsRepository) {
+
+    private var contactsList: LiveData<List<Contact>> = repository.getAllContacts()
+
+    fun insert(contact: Contact) {
+        repository.insert(contact)
+    }
+
+    fun update(contact: Contact) {
+        repository.update(contact)
+    }
+
+    fun deleteContact(contact: Contact) {
+        repository.deleteContact(contact)
+    }
+
+    fun getAllContacts(): LiveData<List<Contact>> {
+        return contactsList
+    }
+}
