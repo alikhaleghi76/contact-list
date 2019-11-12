ad5a9465c92b3fed1721bc8a0eee2925683e1eb0
Ali Khaleghi <alikhaleghi76@gmail.com>
2019-11-12 20:54:50 +0330

    implement architecture base

---
+++ app/src/main/java/ali/khaleghi/contactlist/CoreApplication.kt
@@ -0,0 +1,7 @@
+package ali.khaleghi.contactlist
+
+import android.app.Application
+
+class CoreApplication: Application() {
+
+}
--- app/src/main/java/ali/khaleghi/contactlist/view/activity/ListActivity.kt
+++ app/src/main/java/ali/khaleghi/contactlist/view/activity/ListActivity.kt
@@ -1,12 +1,13 @@
-package ali.khaleghi.contactlist
+package ali.khaleghi.contactlist.view.activity
 
+import ali.khaleghi.contactlist.R
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class ListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_list)
     }
 }
